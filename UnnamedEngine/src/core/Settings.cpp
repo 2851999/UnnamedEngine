27266ae01148f2630424895648ca0292d95c90f8
2851999 <Joel@TheBeast>
2015-12-03 16:31:48 +0000

    * Added changelog *

--- UnnamedEngine/src/core/Settings.cpp
+++ UnnamedEngine/src/core/Settings.cpp
@@ -23,9 +23,9 @@
  ***************************************************************************************************/
 
 const char* Settings::ENGINE_NAME =         "Unnamed Engine";
-const char* Settings::ENGINE_VERSION =      "V0.1.2";
+const char* Settings::ENGINE_VERSION =      "V0.1.3";
 const char* Settings::ENGINE_VERSION_NAME = "New Language";
-const char* Settings::ENGINE_DATE =         "02/12/2015";
+const char* Settings::ENGINE_DATE =         "03/12/2015";
 const char* Settings::ENGINE_BUILD =        "Experimental";
 
 /***************************************************************************************************/
